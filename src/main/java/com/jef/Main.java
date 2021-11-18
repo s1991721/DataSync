f7215a1070990babfd647a79eb103c8e4e5990c8
Jef <278971585@qq.com>
2021-11-18 14:16:28 +0800

    struct change

--- src/main/java/com/jef/Main.java
+++ src/main/java/com/jef/Main.java
@@ -16,62 +16,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            copyFile(new HashMap<>());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("hello world");
     }
 
-    public static void copyFile(Map<String, String> params) throws IOException {
-        File inFile = new File("D:\\GitHub\\DataSync\\src\\main\\resources\\test.ktr");
-        FileReader fileReader = new FileReader(inFile);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-        File outFile = new File("D:\\GitHub\\DataSync\\src\\main\\resources\\test1.ktr");
-        FileWriter fileWriter = new FileWriter(outFile);
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
-        String line = null;
-        while ((line = bufferedReader.readLine()) != null) {
-            String newLine = replaceLine(line, params);
-
-            bufferedWriter.write(newLine);
-            bufferedWriter.newLine();
-        }
-        bufferedReader.close();
-        fileReader.close();
-
-        bufferedWriter.close();
-        fileWriter.close();
-    }
-
-    public static String replaceLine(String line, Map<String, String> params) {
-
-        int a = line.indexOf("${");
-        int b = line.indexOf("}$");
-        if (a < 0) {
-            return line;
-        }
-
-        String key = line.substring(a + 2, b);
-        System.out.println(a + ":" + b + "---->" + key);
-        String value = params.getOrDefault(key, "${null}$");
-        line = line.replace("${" + key + "}$", value);
-
-        return line;
-    }
-
-    private void trans() {
-        try {
-            KettleEnvironment.init();
-            TransMeta transMeta = new TransMeta("D:\\test.ktr");
-            Trans trans = new Trans(transMeta);
-            trans.execute(null);
-            trans.waitUntilFinished();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
