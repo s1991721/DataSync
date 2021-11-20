5c32eca8d6de3dd650428c5bec8113319c8d94dd
Jef <278971585@qq.com>
2021-11-20 11:58:20 +0800

    department content processor

---
+++ src/main/java/com/jef/datasync/thread/Worker.java
@@ -0,0 +1,47 @@
+package com.jef.datasync.thread;
+
+import com.jef.datasync.adapter.Adapter;
+import com.jef.datasync.base.BaseDepartment;
+import com.jef.datasync.base.BaseUser;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @Author: Jef
+ * @Date: 2021/11/19 17:52
+ * @Description
+ */
+public class Worker implements Runnable {
+
+    private BaseDepartment department;
+
+    private Adapter adapter;
+
+    private CountDownLatch doneCountDownLatch;
+
+    public Worker(BaseDepartment department, Adapter adapter, CountDownLatch doneCountDownLatch) {
+        this.department = department;
+        this.adapter = adapter;
+        this.doneCountDownLatch = doneCountDownLatch;
+    }
+
+    @Override
+    public void run() {
+        List<BaseUser> userList = adapter.getUserList(department.getId());
+        for (BaseUser user : userList) {
+            insertUpdateUser(user);
+            insertRelation(user);
+        }
+        doneCountDownLatch.countDown();
+    }
+
+    private void insertUpdateUser(BaseUser user) {
+
+    }
+
+    private void insertRelation(BaseUser user) {
+
+    }
+
+}
