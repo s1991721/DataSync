44efbee5d31948146c43dbda8f06bbafd4fd1565
Jef <278971585@qq.com>
2021-11-20 17:43:12 +0800

    ORM init

---
+++ src/main/java/com/jef/datasync/mapper/DepartmentMapper.java
@@ -0,0 +1,12 @@
+package com.jef.datasync.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jef.datasync.base.BaseDepartment;
+
+/**
+ * @Author: Jef
+ * @Date: 2021/11/20 17:32
+ * @Description
+ */
+public interface DepartmentMapper extends BaseMapper<BaseDepartment> {
+}
---
+++ src/main/java/com/jef/datasync/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.jef.datasync.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jef.datasync.base.BaseDepartment;
+import com.jef.datasync.base.BaseUser;
+
+/**
+ * @Author: Jef
+ * @Date: 2021/11/20 17:33
+ * @Description
+ */
+public interface UserMapper  extends BaseMapper<BaseUser> {
+}
