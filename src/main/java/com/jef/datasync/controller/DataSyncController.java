45cc67e66882f05885bd099fe410cd266d8d35ce
Jef <278971585@qq.com>
2021-11-20 17:09:22 +0800

    send msg

--- src/main/java/com/jef/datasync/controller/DataSyncController.java
+++ src/main/java/com/jef/datasync/controller/DataSyncController.java
@@ -2,6 +2,7 @@ package com.jef.datasync.controller;
 
 import com.jef.datasync.kettle.KettleUtils;
 import com.jef.datasync.manager.Coordinate;
+import com.jef.datasync.mq.SimpleProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,16 +22,21 @@ public class DataSyncController {
     @Autowired
     private Coordinate coordinate;
 
+    @Autowired
+    private SimpleProducer producer;
+
     @GetMapping("/syncDataFromThird")
     public String syncDataFromThird(String adapterId) {
         coordinate.startFullSync(adapterId);
+
+        producer.sendDirectMsg("同步三方数据成功");
         return "";
     }
 
     @GetMapping("/syncDataToService")
     public String syncDataToService(String templateId, Map<String, String> params) {
         KettleUtils.trans();
-
+        producer.sendDirectMsg("同步数据至业务服务成功");
         return "";
     }
 
