330eec30630912ac0e6e151f38dcd56582f2179c
Jef <278971585@qq.com>
2021-11-20 11:55:27 +0800

    adapter function

--- src/main/java/com/jef/datasync/adapter/Adapter.java
+++ src/main/java/com/jef/datasync/adapter/Adapter.java
@@ -1,9 +1,27 @@
 package com.jef.datasync.adapter;
 
+import com.jef.datasync.base.BaseDepartment;
+import com.jef.datasync.base.BaseUser;
+
+import java.util.List;
+
 /**
  * @Author: Jef
  * @Date: 2021/11/18 14:18
  * @Description 同步数据的适配器
  */
 public interface Adapter {
+
+    //唯一标识
+    String getId();
+
+    //获取根组织数据数据接口
+    List<BaseDepartment> getDepartmentList(String deptId);
+
+    //获取节点数据数据接口
+    List<BaseUser> getUserList(String deptId);
+
+    //数据变动监听
+    void onDataChange(String dataJson);
+
 }
---
+++ src/main/java/com/jef/datasync/adapter/DingDingAdapter.java
@@ -0,0 +1,33 @@
+package com.jef.datasync.adapter;
+
+import com.jef.datasync.base.BaseDepartment;
+import com.jef.datasync.base.BaseUser;
+
+import java.util.List;
+
+/**
+ * @Author: Jef
+ * @Date: 2021/11/18 16:29
+ * @Description 默认钉钉实现
+ */
+public class DingDingAdapter implements Adapter {
+    @Override
+    public String getId() {
+        return "DingDingAdapter";
+    }
+
+    @Override
+    public List<BaseDepartment> getDepartmentList(String deptId) {
+        return null;
+    }
+
+    @Override
+    public List<BaseUser> getUserList(String deptId) {
+        return null;
+    }
+
+    @Override
+    public void onDataChange(String dataJson) {
+        System.out.println("有数据变更" + dataJson);
+    }
+}
