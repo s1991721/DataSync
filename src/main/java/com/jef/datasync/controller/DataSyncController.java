9d7bc6f80ab4adce298e257028f1a7b850fb102e
Jef <278971585@qq.com>
2021-11-20 15:53:31 +0800

    MQ init

--- src/main/java/com/jef/datasync/controller/DataSyncController.java
+++ src/main/java/com/jef/datasync/controller/DataSyncController.java
@@ -2,7 +2,6 @@ package com.jef.datasync.controller;
 
 import com.jef.datasync.kettle.KettleUtils;
 import com.jef.datasync.manager.Coordinate;
-import com.jef.datasync.mq.MQUtilts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,18 +21,16 @@ public class DataSyncController {
     @Autowired
     private Coordinate coordinate;
 
-    @GetMapping()
+    @GetMapping("/syncDataFromThird")
     public String syncDataFromThird(String adapterId) {
         coordinate.startFullSync(adapterId);
-        MQUtilts.sendMsg("同步三方数据成功");
         return "";
     }
 
-    @GetMapping()
+    @GetMapping("/syncDataToService")
     public String syncDataToService(String templateId, Map<String, String> params) {
         KettleUtils.trans();
 
-        MQUtilts.sendMsg("同步数据至业务服务成功");
         return "";
     }
 
---
+++ src/main/java/com/jef/datasync/mq/SimpleProducer.java
@@ -0,0 +1,22 @@
+package com.jef.datasync.mq;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by mr.lin on 2020/4/24
+ * 简单分发模式，发送者
+ */
+@Component
+public class SimpleProducer {
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendDirectMsg(String msg) {
+        rabbitTemplate.convertAndSend(DirectExchangeConfig.DIRECT_EXCHANGE_NAME, DirectExchangeConfig.DIRECT_ROUTING_KEY, msg);
+    }
+
+}
