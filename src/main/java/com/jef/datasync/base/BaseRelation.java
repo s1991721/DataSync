06f2d51b00c93a938acc356e07f944a614105813
Jef <278971585@qq.com>
2021-11-22 11:43:49 +0800

    添加关系类

---
+++ src/main/java/com/jef/datasync/base/BaseRelation.java
@@ -0,0 +1,29 @@
+package com.jef.datasync.base;
+
+/**
+ * @Author: Jef
+ * @Date: 2021/11/22 11:42
+ * @Description
+ */
+public class BaseRelation {
+
+    private String departmentId;
+
+    private String userId;
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
