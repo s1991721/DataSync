b74f712b7a7a79ac954d09b59e79f6c30226730d
Jef <278971585@qq.com>
2021-11-23 12:21:53 +0800

    模板文件管理功能

--- src/main/java/com/jef/datasync/controller/DataSyncController.java
+++ src/main/java/com/jef/datasync/controller/DataSyncController.java
@@ -2,7 +2,6 @@ package com.jef.datasync.controller;
 
 import com.jef.datasync.kettle.KettleUtils;
 import com.jef.datasync.manager.Coordinate;
-import com.jef.datasync.mq.SimpleProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,23 +18,24 @@ import java.util.Map;
 @RequestMapping("/dataSync")
 public class DataSyncController {
 
+
     @Autowired
     private Coordinate coordinate;
 
     @Autowired
-    private SimpleProducer producer;
+    private KettleUtils kettleUtils;
+
 
     @GetMapping("/syncDataFromThird")
     public String syncDataFromThird(String adapterId) {
         coordinate.startFullSync(adapterId);
-        return "already stared , listen to the message";
+        return "syncDataFromThird already stared , listen to the message";
     }
 
     @GetMapping("/syncDataToService")
     public String syncDataToService(String templateId, Map<String, String> params) {
-        KettleUtils.trans();
-        producer.sendDirectMsg("同步数据至业务服务成功");
-        return "";
+        kettleUtils.trans(templateId, params);
+        return "syncDataToService already stared , listen to the message";
     }
 
 
--- src/main/java/com/jef/datasync/kettle/KettleUtils.java
+++ src/main/java/com/jef/datasync/kettle/KettleUtils.java
@@ -1,27 +1,78 @@
 package com.jef.datasync.kettle;
 
+import com.jef.datasync.mq.SimpleProducer;
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @Author: Jef
  * @Date: 2021/11/18 14:15
  * @Description
  */
+@Component
 public class KettleUtils {
 
+    @Autowired
+    private SimpleProducer producer;
 
-    private static void copyFile(Map<String, String> params) throws IOException {
-        File inFile = new File("D:\\GitHub\\DataSync\\src\\main\\resources\\test.ktr");
-        FileReader fileReader = new FileReader(inFile);
+    public void trans(String templateId, Map<String, String> params) {
+        File source = null;
+        try {
+            source = getTemplate(templateId);
+        } catch (IOException e) {
+            //todo 模板不存在
+            e.printStackTrace();
+            return;
+        }
+
+        Random random = new Random();
+        File target = new File("template" + random.nextInt(50));
+        if (null != source) {
+            try {
+                copyFile(source, target, params);
+            } catch (IOException e) {
+                //todo 模板参数化出错
+                e.printStackTrace();
+                return;
+            }
+        } else {
+            //todo 出错
+            return;
+        }
+
+        try {
+            KettleEnvironment.init();
+            TransMeta transMeta = new TransMeta(target.getAbsolutePath());
+            Trans trans = new Trans(transMeta);
+            trans.execute(null);
+            trans.waitUntilFinished();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        producer.sendDirectMsg("同步数据至业务服务成功");
+        deleteFile(target);
+    }
+
+    public File getTemplate(String templateId) throws IOException {
+        ClassPathResource classPathResource = new ClassPathResource("template/" + templateId);
+        return classPathResource.getFile();
+    }
+
+    private void copyFile(File source, File target, Map<String, String> params) throws IOException {
+        FileReader fileReader = new FileReader(source);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        File outFile = new File("D:\\GitHub\\DataSync\\src\\main\\resources\\test1.ktr");
-        FileWriter fileWriter = new FileWriter(outFile);
+        FileWriter fileWriter = new FileWriter(target);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
         String line = null;
@@ -38,7 +89,7 @@ public class KettleUtils {
         fileWriter.close();
     }
 
-    private static String replaceLine(String line, Map<String, String> params) {
+    private String replaceLine(String line, Map<String, String> params) {
 
         int a = line.indexOf("${");
         int b = line.indexOf("}$");
@@ -54,21 +105,8 @@ public class KettleUtils {
         return line;
     }
 
-    public static void trans() {
-        try {
-            KettleEnvironment.init();
-            TransMeta transMeta = new TransMeta("D:\\test.ktr");
-            Trans trans = new Trans(transMeta);
-            trans.execute(null);
-            trans.waitUntilFinished();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void deleteFile() {
-        File file = new File("D:\\GitHub\\DataSync\\src\\main\\resources\\test1.ktr");
+    private void deleteFile(File file) {
         file.delete();
     }
+
 }
