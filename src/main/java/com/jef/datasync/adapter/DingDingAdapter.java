cc66c4ad4cdce76eb34969711c016850fe8b31d8
Jef <278971585@qq.com>
2021-11-24 17:35:58 +0800

    模拟三方数据

--- src/main/java/com/jef/datasync/adapter/DingDingAdapter.java
+++ src/main/java/com/jef/datasync/adapter/DingDingAdapter.java
@@ -3,6 +3,7 @@ package com.jef.datasync.adapter;
 import com.jef.datasync.base.BaseDepartment;
 import com.jef.datasync.base.BaseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,12 +19,47 @@ public class DingDingAdapter extends BaseAdapter {
 
     @Override
     public List<BaseDepartment> getDepartmentList(String deptId) {
-        return null;
+        BaseDepartment department = new BaseDepartment();
+        department.setName("父部门");
+        department.setId("A");
+        department.setParentId("0");
+
+        BaseDepartment department1 = new BaseDepartment();
+        department.setName("子部门1");
+        department.setId("A1");
+        department.setParentId("A");
+
+        BaseDepartment department2 = new BaseDepartment();
+        department.setName("子部门2");
+        department.setId("A2");
+        department.setParentId("A");
+
+        List<BaseDepartment> list = new ArrayList<>(3);
+        list.add(department);
+        list.add(department1);
+        list.add(department2);
+
+        return list;
     }
 
     @Override
     public List<BaseUser> getUserList(String deptId) {
-        return null;
+        List<BaseUser> list = new ArrayList<>(2);
+
+        if ("A".equals(deptId)) {
+            BaseUser user = new BaseUser();
+            user.setId("111");
+            user.setName("张三");
+
+            BaseUser user1 = new BaseUser();
+            user.setId("222");
+            user.setName("李四");
+
+            list.add(user);
+            list.add(user1);
+        }
+
+        return list;
     }
 
     @Override
